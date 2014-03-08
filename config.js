54c63bd2b1c520dc3734d67966d8d0f3e4d21d26
Olivier Penhoat <openhoat@gmail.com>
2014-03-08 09:46:13 +0100

    debug nodeshub

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
-
 // Feel free to match your settings
 var config = {
-  plugins:{
-    'mvc':{
-      type:'mvc',
-      config:{
-        port:3005,
-        renderFormats:['html'],
-        locales:['en', 'fr']
+  plugins: {
+    'mvc': {
+      type: 'mvc',
+      config: {
+        port: process.env['NODE_SOCKET_PATH'] || 3005,
+        renderFormats: ['html'],
+        locales: ['en', 'fr']
       }
     },
-    'web-sockets':{
-      type:'web-sockets'
+    'web-sockets': {
+      type: 'web-sockets'
     }
   }
 };
