2563a91bb265eaac249dee53110e489e4142335d
Olivier Penhoat <openhoat@gmail.com>
2014-03-11 22:28:17 +0100

    debug

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var HwExpress = require('hw-express')
   , WsServer = require('./lib/ws-server')
   , hwe, wsServer;
 
-hwe = new HwExpress();
+hwe = new HwExpress({
+  'express': require('express'),
+  'ejs': require('ejs'),
+  'less-middleware': require('less-middleware'),
+  'hw-logger': require('hw-logger')
+});
 hwe.start();
 wsServer = new WsServer(hwe);
 wsServer.start();
